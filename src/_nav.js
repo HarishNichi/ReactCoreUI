a4296157a446f42dfe3837483200ce6f9b9f2f32
Harish Devarayal <52703157+HarishNichi@users.noreply.github.com>
2024-05-29 18:21:22 +0530

    Update _nav.js

--- src/_nav.js
+++ src/_nav.js
@@ -8,9 +8,18 @@ const _nav = [
   },
   {
     component: CNavItem,
-    name: '現場名',  //現場名まとめ
-    to: '/dashboard',
-    // name: 'Dashboard Summary'
+    name: '道具概要', // Tool Overview
+    to: '/tool-overview',
+  },
+  {
+    component: CNavItem,
+    name: '現場報告', // Site Report
+    to: '/site-report',
+  },
+  {
+    component: CNavItem,
+    name: '道具のまとめ', // Tool Summary
+    to: '/tool-summary',
   },
   {
     component: CNavTitle,
@@ -19,86 +28,103 @@ const _nav = [
   {
     component: CNavItem,
     name: 'スーパーボックス', // Super Box
-    to: '/tool/superbox',
+    to: '/super-box',
   },
   {
     component: CNavItem,
-    name: 'セットアップ', // Setup
-    to: '/tool/setup',
-  },
-  {
-    component: CNavGroup,
-    name: 'カテゴリー', // Category
-    to: '/tool/category',
-    items: [
-      {
-        component: CNavItem,
-        name: '子1', // Child 1
-        to: '/tool/category/child1',
-      },
-      {
-        component: CNavItem,
-        name: '子2', // Child 2
-        to: '/tool/category/child2',
-      },
-      {
-        component: CNavItem,
-        name: '子3', // Child 3
-        to: '/tool/category/child3',
-      },
-    ],
+    name: 'インパクト', // Impact
+    to: '/impact',
   },
   {
     component: CNavItem,
-    name: '一覧', // List
-    to: '/tool/list',
+    name: 'セーバー', // Saver
+    to: '/saver',
   },
   {
     component: CNavItem,
-    name: '履歴', // History
-    to: '/tool/history',
+    name: 'サンダー', // Sander
+    to: '/sander',
   },
   {
     component: CNavItem,
-    name: '検索', // Search
-    to: '/tool/search',
+    name: '電動ドリル', // Electric Drill
+    to: '/electric-drill',
   },
   {
     component: CNavItem,
-    name: 'トレンド', // Trends
-    to: '/tool/trends',
+    name: '脚立', // Stepladder
+    to: '/stepladder',
   },
   {
-    component: CNavGroup,
-    name: 'チェックリスト', // Checklist
-    to: '/tool/checklist',
-    items: [
-      {
-        component: CNavItem,
-        name: '子1', // Child 1
-        to: '/tool/checklist/child1',
-      },
-      {
-        component: CNavItem,
-        name: '子2', // Child 2
-        to: '/tool/checklist/child2',
-      },
-    ],
+    component: CNavItem,
+    name: 'ボードカッター', // Board Cutter
+    to: '/board-cutter',
   },
-  // {
-  //   component: CNavTitle,
-  //   name: 'ACCOUNT',
-  // },
-  // {
-  //   component: CNavItem,
-  //   name: 'ログアウト', // Logout
-  //   to: '/logout',
-  // },
-  // {
-  //   component: CNavItem,
-  //   name: '設定', // Settings
-  //   to: '/settings',
-  // },
+  {
+    component: CNavItem,
+    name: 'マルチカッター', // Multi Cutter
+    to: '/multi-cutter',
+  },
+  {
+    component: CNavItem,
+    name: '丸のこ', // Circular Saw
+    to: '/circular-saw',
+  },
+  {
+    component: CNavItem,
+    name: '丸のこバッテリー', // Circular Saw Battery
+    to: '/circular-saw-battery',
+  },
+  {
+    component: CNavItem,
+    name: '電動タッカー', // Electric Tacker
+    to: '/electric-tacker',
+  },
+  {
+    component: CNavItem,
+    name: '発電機', // Generator
+    to: '/generator',
+  },
+  {
+    component: CNavItem,
+    name: 'ブロワー', // Blower
+    to: '/blower',
+  },
+  {
+    component: CNavItem,
+    name: 'ブロワーバッテリー', // Blower Battery
+    to: '/blower-battery',
+  },
+  {
+    component: CNavItem,
+    name: 'チェンソー', // Chainsaw
+    to: '/chainsaw',
+  },
+  {
+    component: CNavItem,
+    name: 'エンジンカッター', // Engine Cutter
+    to: '/engine-cutter',
+  },
+  {
+    component: CNavItem,
+    name: '掃除機', // Vacuum Cleaner
+    to: '/vacuum-cleaner',
+  },
+  {
+    component: CNavItem,
+    name: '180サンダー', // 180 Sander
+    to: '/180-sander',
+  },
+  {
+    component: CNavItem,
+    name: 'カップかけサンダー', // Cup Grinder
+    to: '/cup-grinder',
+  },
+  {
+    component: CNavItem,
+    name: 'インパクトレンチ', // Impact Wrench
+    to: '/impact-wrench',
+  },
 ]
 
 export default _nav
